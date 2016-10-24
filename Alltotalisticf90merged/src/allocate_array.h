77bef94a98e4472f64077eacd60ca340cd4c0b00
Paolo Lazzari <plazzari@ogs.trieste.it>
2016-10-24 09:59:02 +0200

    :: code cleaning

--- Alltotalisticf90merged/src/allocate_array.h
+++
@@ -1,2 +0,0 @@
-      allocate(CA_dom%m(L,L,L))
-      allocate(CA_dom%n(L,L,L))
--- Alltotalisticf90merged/src/define_array.h
+++
@@ -1,2 +0,0 @@
-INTEGER                   :: D=3! Spatial dimensions matrix RANK
-INTEGER, allocatable,dimension(:,:,:)   :: m, n
