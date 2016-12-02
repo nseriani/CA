ecbdcbcf47e4bf2973dcb26bdd1abe37b3a03378
Paolo Lazzari <plazzari@ogs.trieste.it>
2016-12-02 10:19:34 +0100

    :: added new functions operating on CA_state

--- Alltotalisticf90merged/src/statemem.f90
+++ Alltotalisticf90merged/src/statemem.f90
@@ -4,7 +4,9 @@ type state
 INTEGER                               :: S                  ! size of the  State
 INTEGER, allocatable, dimension(:)    :: ipopulation,ipop, ipopulation0   ! state vectors
 end type state
-type(state),save                      :: CA_state1
+
+type(state),save                      :: CA_state1, CA_state_surf, CA_state_surf0
+
 contains
 
 subroutine init_state(CA_dom,CA_state)
@@ -12,9 +14,9 @@ subroutine init_state(CA_dom,CA_state)
    type(domain) :: CA_dom
    type(state ) :: CA_state
    
-   allocate(CA_state%ipopulation(0:CA_dom%S))
-   allocate(CA_state%ipop(0:CA_dom%S))
-   allocate(CA_state%ipopulation0(0:CA_dom%S))
+   if ( .NOT. allocated(CA_state%ipopulation))  allocate(CA_state%ipopulation(0:CA_dom%S))
+   if ( .NOT. allocated(CA_state%ipop))         allocate(CA_state%ipop(0:CA_dom%S))
+   if ( .NOT. allocated(CA_state%ipopulation0)) allocate(CA_state%ipopulation0(0:CA_dom%S))
 ! initialize 
   CA_state%ipopulation(:) = 0.
   CA_state%ipop(:)        = 0.
@@ -92,4 +94,201 @@ END SELECT
 CLOSE(UNIT=333)
 
 end subroutine
+
+subroutine copy_state(CA_state_in,CA_state_out)
+implicit none
+type(state ), intent(in)   :: CA_state_in
+type(state ), intent(inout):: CA_state_out
+
+CA_state_out%S            = CA_state_in%S
+CA_state_out%ipopulation  = CA_state_in%ipopulation
+CA_state_out%ipop         = CA_state_in%ipop
+CA_state_out%ipopulation0 = CA_state_in%ipopulation0
+
+end subroutine
+
+logical function compare_state(CA_dom, CA_state_in1,CA_state_in2)
+implicit none
+type(domain), intent(in)   :: CA_dom
+type(state ), intent(in)   :: CA_state_in1, CA_state_in2
+!local 
+integer                    :: i        
+
+do i =0, CA_dom%S            
+   if (  CA_state_in1%ipopulation(i) .NE. CA_state_in2%ipopulation(i) ) then
+    compare_state = .FALSE.
+    return
+   endif
+enddo
+
+compare_state = .TRUE.
+return
+end function
+
+subroutine fill_state(CA_dom,CA_state_in)
+use latticemem
+use rulemem
+implicit none
+type(domain), intent(in)      :: CA_dom
+type(state ), intent(inout)   :: CA_state_in
+
+CA_state_in%ipop = CA_state_in%ipopulation
+
+   SELECT CASE (TRIM(CA_dom%latticetype))
+
+      CASE('sc')
+
+           SELECT CASE (TRIM(CA_dom%neighbourhood))
+
+              CASE('vonneumann')
+                  call fill_state_sc_fn(CA_dom,CA_state_in)
+
+              CASE('moore')
+                  call fill_state_sc_mo(CA_dom,CA_state_in)
+
+              CASE DEFAULT
+                  write(*,*) 'Scheme: '//TRIM(CA_dom%latticetype)//'-'//TRIM(CA_dom%latticetype)//' not implemented'
+                  STOP 
+
+           END SELECT
+
+      CASE('hc')
+
+           SELECT CASE (TRIM(CA_dom%neighbourhood))
+
+              CASE('vonneumann')
+                  call fill_state_hc(CA_dom,CA_state_in)
+
+              CASE DEFAULT
+                  write(*,*) 'Scheme: '//TRIM(CA_dom%latticetype)//'-'//TRIM(CA_dom%latticetype)//' not implemented'
+                  STOP 
+
+           END SELECT
+          
+      CASE('fcc')
+
+           SELECT CASE (TRIM(CA_dom%neighbourhood))
+
+              CASE('vonneumann')
+                  write(*,*) 'Scheme: '//TRIM(CA_dom%latticetype)//'-'//TRIM(CA_dom%latticetype)//' not implemented'
+                  STOP 
+
+              CASE DEFAULT
+                  write(*,*) 'Scheme: '//TRIM(CA_dom%latticetype)//'-'//TRIM(CA_dom%latticetype)//' not implemented'
+                  STOP 
+
+           END SELECT
+
+      CASE DEFAULT
+
+           write(*,*) 'Lattice: '//TRIM(CA_dom%latticetype)//' not implemented' 
+           STOP 
+   END SELECT
+
+
+
+end subroutine
+
+subroutine fill_state_sc_fn(CA_dom,CA_state_in)
+use latticemem
+use rulemem
+implicit none
+type(domain), intent(in)      :: CA_dom
+type(state ), intent(inout)   :: CA_state_in
+!     Local variables
+integer i,k
+integer a,b
+
+do i=1,CA_dom%S
+   do k=1,CA_dom%nneigh,2
+
+      a = CA_dom%neighlist(i,k)
+      b = CA_dom%neighlist(i,k+1)
+
+      if ( (CA_state_in%ipopulation(a).EQ.1) .AND. (CA_state_in%ipopulation(b).EQ.1)) then
+
+         CA_state_in%ipop(i)=1
+         exit 
+      endif
+
+   enddo
+enddo
+
+do i=1,CA_dom%S
+   CA_state_in%ipopulation(i) = CA_state_in%ipop(i)
+enddo  
+
+end subroutine
+
+subroutine fill_state_sc_mo(CA_dom,CA_state_in)
+use latticemem
+use rulemem
+implicit none
+type(domain), intent(in)      :: CA_dom
+type(state ), intent(inout)   :: CA_state_in
+!     Local variables
+integer i,k
+integer a,b
+
+do i=1,CA_dom%S
+   do k=1,CA_dom%nneigh/2
+
+      a = CA_dom%neighlist(i,k)
+      b = CA_dom%neighlist(i,CA_dom%nneigh+1-k)
+
+      if ( (CA_state_in%ipopulation(a) .EQ. 1) .AND. ( CA_state_in%ipopulation(b) .EQ.  1) ) then
+         CA_state_in%ipop(i)=1
+         exit
+      endif
+
+   enddo
+enddo
+
+do i=1,CA_dom%S
+   CA_state_in%ipopulation(i) = CA_state_in%ipop(i)
+enddo
+
+end subroutine
+
+subroutine fill_state_hc(CA_dom,CA_state_in)
+use latticemem
+use rulemem
+implicit none
+type(domain), intent(in)      :: CA_dom
+type(state ), intent(inout)   :: CA_state_in
+!     Local variables
+integer i
+integer a,b
+
+do i=1,CA_dom%S
+
+      a = CA_dom%neighlist(i,1)
+      b = CA_dom%neighlist(i,4)
+
+      if ( (CA_state_in%ipopulation(a) .EQ. 1) .AND. ( CA_state_in%ipopulation(b) .EQ.  1) ) then
+         CA_state_in%ipop(i)=1
+      endif
+
+      a = CA_dom%neighlist(i,2)
+      b = CA_dom%neighlist(i,5)
+
+      if ( (CA_state_in%ipopulation(a) .EQ. 1) .AND. ( CA_state_in%ipopulation(b) .EQ.  1) ) then
+         CA_state_in%ipop(i)=1
+      endif
+
+      a = CA_dom%neighlist(i,3)
+      b = CA_dom%neighlist(i,6)
+
+      if ( (CA_state_in%ipopulation(a) .EQ. 1) .AND. ( CA_state_in%ipopulation(b) .EQ.  1) ) then
+         CA_state_in%ipop(i)=1
+      endif
+
+enddo
+
+do i=1,CA_dom%S
+   CA_state_in%ipopulation(i) = CA_state_in%ipop(i)
+enddo
+
+end subroutine
+
 end module
